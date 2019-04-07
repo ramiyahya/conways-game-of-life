0d9158fbdf889d0ca3e5377fef13c6ffb674216e
Rami Yahya <contact@ramiyahya.com>
2019-04-07 17:44:44 +1000

    fix: array.fill warning

--- src/utils/createGrid.ts
+++ src/utils/createGrid.ts
@@ -1,7 +1,7 @@
 import { DEAD_CELL } from '../constants';
 
 const createGrid = (rows, cols, seed = DEAD_CELL) => {
-    const emptyArray = new Array(rows).fill();
+    const emptyArray = new Array(rows).fill([]);
     const grid = emptyArray.map(() => new Array(cols).fill(seed));
 
     return {

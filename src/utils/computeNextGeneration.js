82e136c7300f63ea66ad8f850848e07f2707a975
Rami Yahya <contact@ramiyahya.com>
2019-04-07 17:34:37 +1000

    feat: implement `computeNextGeneration` and `createGrid`

--- src/utils/computeNextGeneration.js
+++ src/utils/computeNextGeneration.js
@@ -1,5 +1,16 @@
-const computeNextGeneration = () => {
-    return;
+import { LIVE_CELL, DEAD_CELL } from '../constants';
+
+const computeNextGeneration = (currentState, liveNeighbours) => {
+    if (currentState === DEAD_CELL && liveNeighbours === 3) {
+        return LIVE_CELL;
+    } else if (
+        currentState === LIVE_CELL &&
+        (liveNeighbours < 2 || liveNeighbours > 3)
+    ) {
+        return DEAD_CELL;
+    } else {
+        return currentState;
+    }
 };
 
 export { computeNextGeneration };
--- src/utils/createGrid.js
+++ src/utils/createGrid.js
@@ -1,5 +1,14 @@
-const createGrid = () => {
-    return;
+import { DEAD_CELL } from '../constants';
+
+const createGrid = (rows, cols, seed = DEAD_CELL) => {
+    const emptyArray = new Array(rows).fill();
+    const grid = emptyArray.map(() => new Array(cols).fill(seed));
+
+    return {
+        grid,
+        rows,
+        cols
+    };
 };
 
 export { createGrid };
